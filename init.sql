af84786cf0cd4d577bf8d0ab634330ccbd4afcc3
ThiagoSantos21 <thiagolima-santos@hotmail.com>
2025-05-31 17:09:44 -0300

    (config): correção na relação entre pessoa e usuário

--- init.sql
+++ init.sql
@@ -1,6 +1,30 @@
 /* Script de inicialização do banco de dados */
 
--- Criação dos usuários
+CREATE TABLE tipo_usuario(
+    nome VARCHAR(50) PRIMARY KEY
+);
+
+CREATE TABLE combustivel(
+    nome VARCHAR(50) PRIMARY KEY
+);
+
+CREATE TABLE bandeira(
+    nome VARCHAR(50) PRIMARY KEY,
+    url VARCHAR(50)
+);
+
+CREATE TABLE cidade(
+    nome VARCHAR(50) PRIMARY KEY,
+    estado VARCHAR(50) UNIQUE NOT NULL,
+    latitude DECIMAL(10, 8),
+    longitude DECIMAL(11, 8)
+);
+
+CREATE TABLE bairro(
+    nome VARCHAR(50) PRIMARY KEY,
+    nomeCidade VARCHAR(50),
+    FOREIGN KEY (nomeCidade) REFERENCES cidade(nome)
+);
 
 CREATE TABLE usuario (
     login VARCHAR(50) PRIMARY KEY,
@@ -10,43 +34,14 @@ CREATE TABLE usuario (
 );
 
 CREATE TABLE pessoa (
-    login VARCHAR(50), -- Perguntar para a professora se esse realmente precisa ser chave primária
+    login VARCHAR(50) PRIMARY KEY,
     nome VARCHAR(50),
     endereco VARCHAR(50),
     nomeBairro VARCHAR(50),
-    FOREIGN KEY (nomeBairro) REFERENCES bairro(nome)
+    loginUsuario VARCHAR(50),
+    FOREIGN KEY (nomeBairro) REFERENCES bairro(nome),
     FOREIGN KEY (login) REFERENCES usuario(login)
 );
-
-CREATE TABLE tipo_usuario(
-    nome VARCHAR(50) PRIMARY KEY
-);
-
-CREATE TABLE comentario(
-    momento DATE,
-    loginPessoa VARCHAR(50), 
-    cnpjPosto VARCHAR(14),
-    FOREIGN KEY (loginPessoa) REFERENCES Pessoa(login),
-    FOREIGN KEY (cnpjPosto) REFERENCES Posto(cnpj)
-);
-
-CREATE TABLE veiculo(
-    placa VARCHAR(7) PRIMARY KEY,
-    marca VARCHAR(50),
-    modelo VARCHAR(50),
-    loginUsuario VARCHAR(50),
-    FOREIGN KEY (loginUsuario) REFERENCES usuario(login)
-);
-
-CREATE TABLE veiculo_abastecido_combustivel(
-    placaVeiculo VARCHAR(7),
-    nomeCombustivel VARCHAR(50),
-    PRIMARY KEY (placaVeiculo, nomeCombustivel),
-    FOREIGN KEY (placaVeiculo) REFERENCES veiculo(placa),
-    FOREIGN KEY (nomeCombustivel) REFERENCES combustivel(nome)
-);
-
--- Criação dos postos
 
 CREATE TABLE posto(
     cnpj VARCHAR(14) PRIMARY KEY,
@@ -62,13 +57,28 @@ CREATE TABLE posto(
     FOREIGN KEY (nomeBairro) REFERENCES bairro(nome)
 );
 
-CREATE TABLE bandeira(
-    nome VARCHAR(50) PRIMARY KEY,
-    url VARCHAR(50)
+CREATE TABLE comentario(
+    momento DATE,
+    loginPessoa VARCHAR(50), 
+    cnpjPosto VARCHAR(14),
+    FOREIGN KEY (loginPessoa) REFERENCES pessoa(login),
+    FOREIGN KEY (cnpjPosto) REFERENCES posto(cnpj)
 );
 
-CREATE TABLE combustivel(
-    nome VARCHAR(50) PRIMARY KEY
+CREATE TABLE veiculo(
+    placa VARCHAR(7) PRIMARY KEY,
+    marca VARCHAR(50),
+    modelo VARCHAR(50),
+    loginPessoa VARCHAR(50),
+    FOREIGN KEY (loginPessoa) REFERENCES pessoa(login)
+);
+
+CREATE TABLE veiculo_abastecido_combustivel(
+    placaVeiculo VARCHAR(7),
+    nomeCombustivel VARCHAR(50),
+    PRIMARY KEY (placaVeiculo, nomeCombustivel),
+    FOREIGN KEY (placaVeiculo) REFERENCES veiculo(placa),
+    FOREIGN KEY (nomeCombustivel) REFERENCES combustivel(nome)
 );
 
 CREATE TABLE preco(
@@ -86,20 +96,4 @@ CREATE TABLE posto_combustivel(
     PRIMARY KEY (cnpjPosto, nomeCombustivel),
     FOREIGN KEY (nomeCombustivel) REFERENCES combustivel(nome),
     FOREIGN KEY (cnpjPosto) REFERENCES posto(cnpj)
-);
-
-
--- Criação dos locais
-
-CREATE TABLE bairro(
-    nome VARCHAR(50) PRIMARY KEY,
-    nomeCidade VARCHAR(50),
-    FOREIGN KEY (nomeCidade) REFERENCES cidade(nome)
-);
-
-CREATE TABLE cidade(
-    nome VARCHAR(50) PRIMARY KEY,
-    estado VARCHAR(50) UNIQUE NOT NULL,
-    latitude DECIMAL(10, 8),
-    longitude DECIMAL(11, 8)
 );
