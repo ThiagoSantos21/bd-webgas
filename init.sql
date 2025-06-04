dc498c3a315d13d07df5386d9d5f2297703f6420
ThiagoSantos21 <thiagolima-santos@hotmail.com>
2025-06-03 21:47:23 -0300

    (feat, fix): ajuste na criação de uma tabela e inserção de dados

--- init.sql
+++ init.sql
@@ -1,99 +1,122 @@
 /* Script de inicialização do banco de dados */
 
+-- inserts feitos
 CREATE TABLE tipo_usuario(
-    nome VARCHAR(50) PRIMARY KEY
+    nome VARCHAR(255) PRIMARY KEY
 );
 
+-- inserts feitos
 CREATE TABLE combustivel(
-    nome VARCHAR(50) PRIMARY KEY
+    nome VARCHAR(255) PRIMARY KEY
 );
 
+-- inserts feitos
 CREATE TABLE bandeira(
-    nome VARCHAR(50) PRIMARY KEY,
-    url VARCHAR(50)
+    nome VARCHAR(255) PRIMARY KEY,
+    url VARCHAR(255)
 );
 
+-- inserts feitos
 CREATE TABLE cidade(
-    nome VARCHAR(50) PRIMARY KEY,
-    estado VARCHAR(50) UNIQUE NOT NULL,
+    nome VARCHAR(255) PRIMARY KEY,
+    estado VARCHAR(255) NOT NULL,
     latitude DECIMAL(10, 8),
     longitude DECIMAL(11, 8)
 );
 
+-- inserts feitos
 CREATE TABLE bairro(
-    nome VARCHAR(50) PRIMARY KEY,
-    nomeCidade VARCHAR(50),
+    nome VARCHAR(255) PRIMARY KEY,
+    nomeCidade VARCHAR(255),
     FOREIGN KEY (nomeCidade) REFERENCES cidade(nome)
 );
 
+-- inserts feitos
 CREATE TABLE usuario (
-    login VARCHAR(50) PRIMARY KEY,
-    senha VARCHAR(50),
-    nomeTipoUsuario VARCHAR(50),
+    login VARCHAR(255) PRIMARY KEY,
+    senha VARCHAR(255),
+    nomeTipoUsuario VARCHAR(255),
     FOREIGN KEY (nomeTipoUsuario) REFERENCES tipo_usuario(nome)
 );
 
+-- inserts feitos
 CREATE TABLE pessoa (
-    login VARCHAR(50) PRIMARY KEY,
-    nome VARCHAR(50),
-    endereco VARCHAR(50),
-    nomeBairro VARCHAR(50),
-    loginUsuario VARCHAR(50),
+    login VARCHAR(255) PRIMARY KEY,
+    nome VARCHAR(255),
+    endereco VARCHAR(255),
+    nomeBairro VARCHAR(255),
+    loginUsuario VARCHAR(255),
     FOREIGN KEY (nomeBairro) REFERENCES bairro(nome),
     FOREIGN KEY (login) REFERENCES usuario(login)
 );
 
+-- inserts feitos
 CREATE TABLE posto(
     cnpj VARCHAR(14) PRIMARY KEY,
-    razao_social VARCHAR(50),
-    nome_fantasia VARCHAR(50),
+    razao_social VARCHAR(255),
+    nome_fantasia VARCHAR(255),
     latitude DECIMAL(10, 8),
     longitude DECIMAL(11, 8),
-    endereco VARCHAR(50),
+    endereco VARCHAR(255),
     telefone VARCHAR(9),
-    nomeBandeira VARCHAR(50),
-    nomebairro VARCHAR(50),
+    nomeBandeira VARCHAR(255),
+    nomebairro VARCHAR(255),
     FOREIGN KEY (nomeBandeira) REFERENCES bandeira(nome),
     FOREIGN KEY (nomeBairro) REFERENCES bairro(nome)
 );
 
 CREATE TABLE comentario(
     momento DATE,
-    loginPessoa VARCHAR(50), 
+    loginPessoa VARCHAR(255), 
     cnpjPosto VARCHAR(14),
     FOREIGN KEY (loginPessoa) REFERENCES pessoa(login),
     FOREIGN KEY (cnpjPosto) REFERENCES posto(cnpj)
 );
 
+-- inserts feitos
 CREATE TABLE veiculo(
     placa VARCHAR(7) PRIMARY KEY,
-    marca VARCHAR(50),
-    modelo VARCHAR(50),
-    loginPessoa VARCHAR(50),
+    marca VARCHAR(255),
+    modelo VARCHAR(255),
+    loginPessoa VARCHAR(255),
     FOREIGN KEY (loginPessoa) REFERENCES pessoa(login)
 );
 
+-- inserts feitos
 CREATE TABLE veiculo_abastecido_combustivel(
     placaVeiculo VARCHAR(7),
-    nomeCombustivel VARCHAR(50),
+    nomeCombustivel VARCHAR(255),
     PRIMARY KEY (placaVeiculo, nomeCombustivel),
     FOREIGN KEY (placaVeiculo) REFERENCES veiculo(placa),
     FOREIGN KEY (nomeCombustivel) REFERENCES combustivel(nome)
 );
 
+--inserts feitos
 CREATE TABLE preco(
     momento DATETIME PRIMARY KEY,
     valor DECIMAL(5,3),
-    nomeCombustivel VARCHAR(50),
+    nomeCombustivel VARCHAR(255),
     cnpjPosto VARCHAR(14),
     FOREIGN KEY (nomeCombustivel) REFERENCES combustivel(nome),
     FOREIGN KEY (cnpjPosto) REFERENCES posto(cnpj)
 );
 
+-- inserts feitos
 CREATE TABLE posto_combustivel(
-    nomeCombustivel VARCHAR(50),
+    nomeCombustivel VARCHAR(255),
     cnpjPosto VARCHAR(14),
     PRIMARY KEY (cnpjPosto, nomeCombustivel),
     FOREIGN KEY (nomeCombustivel) REFERENCES combustivel(nome),
     FOREIGN KEY (cnpjPosto) REFERENCES posto(cnpj)
+);
+
+CREATE TABLE venda (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    loginPessoa VARCHAR(50),
+    cnpjPosto VARCHAR(14),
+    preco DATETIME,
+    FOREIGN KEY (loginPessoa) REFERENCES pessoa(login),
+    FOREIGN KEY (cnpjPosto) REFERENCES posto(cnpj),
+    FOREIGN KEY (nomeCombustivel) REFERENCES combustivel(nome),
+    FOREIGN KEY (preco) REFERENCES preco(momento)
 );
--- insert.sql
+++ insert.sql
@@ -65,8 +65,46 @@ VALUES
     
 INSERT INTO posto (cnpj, razao_social, nome_fantasia, latitude, longitude, endereco, telefone, nomeBandeira, nomebairro)
 VALUES
-	('11111111111111', 'Posto 1', 'Posto 1', 0,0,'Rua 8','111111111','Shell','Tijuca'),
-    ('22222222222222', 'Posto 2', 'Posto 2', 0,0,'Rua 9','222222222','Vibra','Cambuí'),
-    ('33333333333333', 'Posto 3', 'Posto 3', 0,0,'Rua 10','333333333','Ipiranga','Taquaral'),
-    ('44444444444444', 'Posto 4', 'Posto 4', 0,0,'Rua 11','444444444','Vibra','Moema');
-    
+	('11111111111111', 'Posto 1', 'Posto 1', -22.9025, -22.9064, 'Rua 8', '111111111', 'Shell', 'Tijuca'),
+    ('22222222222222', 'Posto 2', 'Posto 2', -22.9025, -46.6388, 'Rua 9', '222222222', 'Vibra', 'Cambuí'),
+    ('33333333333333', 'Posto 3', 'Posto 3', -23.5479, -47.0616, 'Rua 10', '333333333', 'Ipiranga', 'Taquaral');
+
+INSERT INTO posto_combustivel(cnpjPosto, nomeCombustivel)
+VALUES
+    ('11111111111111', 'Gasolina'),
+    ('11111111111111', 'GNV'),
+    ('11111111111111', 'Álcool'),
+    ('22222222222222', 'Gasolina'),
+    ('22222222222222', 'GNV'),
+    ('22222222222222', 'Álcool'),
+    ('22222222222222', 'Diesel'),
+    ('33333333333333', 'Álcool'),
+    ('33333333333333', 'Diesel'),
+    ('33333333333333', 'GNV');
+
+INSERT INTO preco(momento, valor, cnpjPosto, nomeCombustivel)
+VALUES
+    ('2025-01-15 08:23:45', 5, '11111111111111', 'Gasolina'),
+    ('2025-02-02 14:12:10', 2.56,'11111111111111', 'GNV'),
+    ('2025-03-10 19:45:30', 1.5, '11111111111111', 'Álcool'),
+    ('2025-04-25 06:35:00', 4.87, '22222222222222', 'Gasolina'),
+    ('2025-05-17 22:59:59', 2.44, '22222222222222', 'GNV'),
+    ('2025-06-09 12:00:00', 1.56, '22222222222222', 'Álcool'),
+    ('2025-07-21 09:15:10', 4.11, '22222222222222', 'Diesel'),
+    ('2025-08-30 16:47:25', 1.66, '33333333333333', 'Álcool'),
+    ('2025-10-03 11:11:11', 4.22, '33333333333333', 'Diesel'),
+    ('2025-12-20 20:30:00', 2.51, '33333333333333', 'GNV');
+
+INSERT INTO veiculo_abastecido_combustivel(placaVeiculo, nomeCombustivel)
+VALUES
+    ('aaa11a1', 'Gasolina'),
+    ('aaa11a1', 'Diesel'),
+    ('bbb22b2', 'Gasolina'),
+    ('bbb22b2', 'GNV'),
+    ('ccc33c3', 'Álcool'),
+    ('ccc33c3', 'Diesel'),
+    ('ddd44d4', 'Álcool'),
+    ('ddd44d4', 'GNV'),
+    ('ddd44d4', 'Diesel'),
+    ('eee55e5', 'Gasolina'),
+    ('eee55e5', 'GNV');
